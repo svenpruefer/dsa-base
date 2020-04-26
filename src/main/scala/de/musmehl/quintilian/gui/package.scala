68aa99db5da821e7eb27c14840b48775950d898b
Sven Prüfer <sven@musmehl.de>
2020-04-26 17:20:03 +0200

    Add basic entrance window

---
+++ src/main/scala/de/musmehl/quintilian/gui/package.scala
@@ -0,0 +1,23 @@
+/* Copyright (C) 2020 Sven Prüfer - All Rights Reserved
+ *
+ * This file is part of quintilian.
+ *
+ * quintilian is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * quintilian is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with quintilian.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package de.musmehl.quintilian
+
+/**
+  * Package for all GUI-related parts.
+  */
+package object gui {}
