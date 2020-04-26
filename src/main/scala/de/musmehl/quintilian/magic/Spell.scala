2f00036e1aaae305153efb38069b256c26f301a5
Sven Prüfer <sven@musmehl.de>
2020-04-26 23:42:23 +0200

    Add basic spell trait

---
+++ src/main/scala/de/musmehl/quintilian/magic/Spell.scala
@@ -0,0 +1,33 @@
+/* Copyright (C) 2020 Sven Prüfer - All Rights Reserved
+ *
+ * This file is part of quintilian.
+ *
+ * quintilian is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * quintilian is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with quintilian.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package de.musmehl.quintilian.magic
+
+import de.musmehl.quintilian.character.CharacterState
+
+trait Spell {
+
+  def merkmale: Set[SpellAttribute]
+
+  def executable(characterState: CharacterState): Boolean
+
+  def execute(characterState: CharacterState): (SpellResult, CharacterState)
+}
+
+object Spell {
+  case class Wert(value: Int) extends AnyVal
+}
