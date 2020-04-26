1543af1d9a127629e07b701c60127805dd34b425
Sven Prüfer <sven@musmehl.de>
2020-04-26 23:43:26 +0200

    Add first draft of character state

---
+++ src/main/scala/de/musmehl/quintilian/character/CharacterState.scala
@@ -0,0 +1,29 @@
+/* Copyright (C) 2020 Sven Prüfer - All Rights Reserved
+ *
+ * This file is part of quintilian.
+ *
+ * quintilian is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * quintilian is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with quintilian.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package de.musmehl.quintilian.character
+
+import de.musmehl.quintilian.character.properties.Property.{Eigenschaften, Energien, Kampfwerte}
+
+/**
+  * State of a character at a given point in time.
+  */
+case class CharacterState(
+    baseProperties: Eigenschaften,
+    energies: Energien,
+    battleValues: Kampfwerte
+)
